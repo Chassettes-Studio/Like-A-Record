24d32ee62eefeaef9ebd2f0b568e11c52a3f7909
SunoWhere <sunowhere@gmail.com>
2026-06-20 09:58:28 +0200

    Added base wave manager api

--- core/enemies/enemy.gd
+++ core/enemies/enemy.gd
@@ -3,6 +3,8 @@ extends Resource
 
 @export var name: String
 @export var texture: Texture2D
+@export var min_wave: int = 1
+@export var weight: float = 1
 
 @export var attack: EnemyAttack
 @export var brain: EnemyBrain
