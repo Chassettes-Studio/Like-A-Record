4882ee45befef98e94a7ad00d2ec7686a1df63f5
SunoWhere <sunowhere@gmail.com>
2026-06-22 19:36:04 +0200

    Changed laser_disc behavior to stop player tracking before shooting

--- core/enemies/enemy_brain/laser_disc_brain.gd
+++ core/enemies/enemy_brain/laser_disc_brain.gd
@@ -4,17 +4,20 @@ extends EnemyBrain
 enum BrainState {
 	START,
 	IDLE,
+	SEEKING,
 	LOADING,
 	SHOOTING,
 }
 
 @export var idle_duration: float = 3
-@export var loading_duration: float = 3
+@export var seeking_duration: float = 3
+@export var loading_duration: float = 0.4
 @export var attack: EnemyDiscreteAttack
 
 var current_state: BrainState = BrainState.START
 var state_ended: bool = false
 var beam: Line2D = Line2D.new()
+var marker: Marker2D = Marker2D.new()
 
 
 func change_state(entity: EnemyEntity) -> void:
@@ -23,11 +26,20 @@ func change_state(entity: EnemyEntity) -> void:
 			current_state = BrainState.IDLE
 			entity.get_tree().create_timer(idle_duration).timeout.connect(end_state)
 		BrainState.IDLE:
-			current_state = BrainState.LOADING
+			current_state = BrainState.SEEKING
 			beam = Line2D.new()
 			beam.width = 10
 			beam.default_color = Color.RED
 			entity.add_child(beam)
+			entity.get_tree().create_timer(seeking_duration).timeout.connect(end_state)
+		BrainState.SEEKING:
+			current_state = BrainState.LOADING
+			beam.default_color = Color.BLUE
+			beam.points = []
+			beam.add_point(beam.to_local(entity.global_position))
+			beam.add_point(beam.to_local(entity.target.position))
+			marker = Marker2D.new()
+			marker.global_position = entity.target.position
 			entity.get_tree().create_timer(loading_duration).timeout.connect(end_state)
 		BrainState.LOADING:
 			current_state = BrainState.SHOOTING
@@ -44,11 +56,15 @@ func process(entity: EnemyEntity, delta: float) -> void:
 		change_state(entity)
 	if current_state == BrainState.START:
 		state_ended = true
-	elif current_state == BrainState.LOADING:
+	elif current_state == BrainState.SEEKING:
 		beam.points = []
 		beam.add_point(beam.to_local(entity.global_position))
 		beam.add_point(beam.to_local(entity.target.position))
 	elif current_state == BrainState.SHOOTING:
 		beam.free()
+		var target_temp: Node2D = entity.target
+		entity.target = marker
 		attack.attack(entity)
+		entity.target = target_temp
+		marker.free()
 		state_ended = true
