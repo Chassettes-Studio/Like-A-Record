719d97ac3fc6c959e8d946493b4edc78552e2983
Jossua <jossua.guinot@gmail.com>
2026-06-23 21:37:00 +0200

    ability framework

---
+++ core/abilities/ability_heal.gd
@@ -0,0 +1,8 @@
+class_name HealAbility
+extends AbilityEffect
+
+@export var amount : int = 2
+
+func use(player: Player) -> void:
+	player.health_controller.heal(amount)
+	
--- core/player/character/character.gd
+++ core/player/character/character.gd
@@ -4,5 +4,7 @@ extends Resource
 @export var name : String
 @export var texture : Texture
 
+@export var ability : Ability
+
 @export var intro_song : AudioStream
 @export var loop_song : AudioStream
