d62322a857a11bb7e23beef6a3f3ad9d9a16aa54
RizBleu <mateo.galizzi05@gmail.com>
2026-06-23 19:24:02 +0200

    rand speed

--- core/enemies/enemy_brain/rush_brain.gd
+++ core/enemies/enemy_brain/rush_brain.gd
@@ -4,8 +4,15 @@ extends EnemyBrain
 @export var speed: float = 100.0
 @export var accel: float = 100.0
 
+var speed_rand: float
+var accel_rand: float
+
+func ready() -> void:
+	speed_rand = randf_range(speed * 0.9, speed * 1.1)
+	accel_rand = randf_range(accel * 0.9, accel * 1.1)
+
 func process(entity: EnemyEntity, _delta: float) -> void:
 	entity.movement_controller.direction = entity.global_position.direction_to(entity.target.global_position)
-	entity.movement_controller.max_speed = speed
-	entity.movement_controller.acceleration = accel
+	entity.movement_controller.max_speed = speed_rand
+	entity.movement_controller.acceleration = accel_rand
 	entity.rotation = entity.velocity.angle() + PI/2
