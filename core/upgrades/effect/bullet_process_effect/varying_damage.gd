9e590aeab59c281edfa792025b95adca99652760
SunoWhere <sunowhere@gmail.com>
2026-06-23 17:11:36 +0200

    Removed varying damage

--- core/upgrades/effect/bullet_process_effect/varying_damage.gd
+++
@@ -1,7 +0,0 @@
-class_name VaryingDamage
-extends BulletProcessEffect
-
-@export var damage_evolution_per_second: float = 1
-
-func process(entity: BulletEntity, delta: float) -> void:
-	entity.bullet_data
