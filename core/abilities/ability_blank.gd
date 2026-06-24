f8e6ee352b0c0a66dd1945a2c65a33abd4c6aabc
Jossua <jossua.guinot@gmail.com>
2026-06-24 23:11:09 +0200

    blank

---
+++ core/abilities/ability_blank.gd
@@ -0,0 +1,19 @@
+class_name AbilityBlank
+extends AbilityEffect
+
+@export var shape : Shape2D
+
+func use(player: Player) -> void:
+	
+	var query := PhysicsShapeQueryParameters2D.new()
+	query.transform = player.global_transform
+	query.shape = shape
+	var collisions := player.get_world_2d().direct_space_state.intersect_shape(query)
+	
+	for entry in collisions:
+		var bullet := entry.collider as BulletEntity
+		if bullet && bullet.area_2d.get_collision_mask_value(1):
+			bullet.queue_free()
+
+	
+	
