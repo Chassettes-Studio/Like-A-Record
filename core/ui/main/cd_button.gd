ef220ff6a8a12019d5ae223d61139c42fc30cbeb
RizBleu <mateo.galizzi05@gmail.com>
2026-06-24 22:40:34 +0200

    better main

--- core/ui/main/cd_button.gd
+++ core/ui/main/cd_button.gd
@@ -2,12 +2,14 @@ class_name CdButton
 extends Node2D
 
 @export var character: Character
+@export var icon: Texture2D
 @export var n: int
 @export var hub_music: AudioStreamPlayer
 @onready var audio: AudioStreamPlayer = $AudioStreamPlayer
 @onready var title: Label = $CanvasLayer/NinePatchRect/Title
 @onready var desc: Label = $CanvasLayer/NinePatchRect/Desc
 @onready var popup: NinePatchRect = $CanvasLayer/NinePatchRect
+@onready var cd: Sprite2D = $cd
 
 var mouse_in: bool = false
 var is_selected: bool = false
@@ -17,12 +19,17 @@ var is_selected: bool = false
 signal cd_selected
 
 func _ready() -> void:
-	sprite.modulate = Color(18.892, 18.892, 18.892, 	0.0)
+	sprite.modulate = Color(18.892, 18.892, 18.892, 0.0)
+	cd.texture = icon
 	title.text = character.name
 	desc.text = character.desc
 	audio.stream = character.layers
 	audio.play()
 	audio.volume_db = -100
+
+func _process(_sdelta: float) -> void:
+	if is_selected:
+		cd.rotation += 0.1
 
 func _input(event: InputEvent) -> void:
 	if mouse_in && event is InputEventMouseButton:
@@ -31,7 +38,6 @@ func _input(event: InputEvent) -> void:
 			select()
 
 func select() -> void:
-	sprite.modulate = Color(18.892, 18.892, 18.892, 0.4)
 	Player.static_character = character
 	cd_selected.emit(n)
 	is_selected = true
@@ -48,8 +54,7 @@ func _on_area_2d_mouse_entered() -> void:
 
 func _on_area_2d_mouse_exited() -> void:
 	mouse_in = false
-	if !is_selected:
-		sprite.modulate = Color(18.892, 18.892, 18.892, 0.0)
+	sprite.modulate = Color(18.892, 18.892, 18.892, 0.0)
 	var tween: Tween = get_tree().create_tween()
 	tween.set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_QUAD)
 	tween.set_parallel(true).set_pause_mode(Tween.TWEEN_PAUSE_PROCESS)
