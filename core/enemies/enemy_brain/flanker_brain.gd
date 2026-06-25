20154cd11ea343537f80170ea0f5876c2cc1efaa
jossua <jossua@flexio.fr>
2026-06-25 18:27:08 +0200

    vla les tweaks

--- core/enemies/enemy_brain/flanker_brain.gd
+++ core/enemies/enemy_brain/flanker_brain.gd
@@ -1,7 +1,7 @@
 class_name FlankerBrain
 extends EnemyBrain
 
-@export var attack: EnemyContinuousAttack
+@export var attack: EnemyDiscreteAttack
 @export var speed: float = 100.0
 @export var accel: float = 100.0
 @export var flanking_range : float = 300
@@ -20,11 +20,11 @@ func get_target_point(entity: EnemyEntity) -> Vector2:
 	return entity.target.global_position + entity.global_position.direction_to(entity.target.global_position).rotated(PI/2) * flanking_range
 
 
-func process(entity: EnemyEntity, delta: float) -> void:
+func process(entity: EnemyEntity, _delta: float) -> void:
 
 		entity.movement_controller.direction = entity.global_position.direction_to(get_target_point(entity))
 		entity.movement_controller.max_speed = speed_rand
 		entity.movement_controller.acceleration = accel_rand
 
 		if entity.global_position.distance_to(entity.target.global_position) < abs(flanking_range) * 2:
-			attack.attack(delta, entity)
+			attack.attack(entity)
