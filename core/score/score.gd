631f90c2dbb7c2041962ffa5eb7343180610c68b
Jossua <jossua.guinot@gmail.com>
2026-06-25 22:36:33 +0200

    scorepersist

--- core/score/score.gd
+++ core/score/score.gd
@@ -6,11 +6,27 @@ var wave: int = 1
 
 signal score_changed
 
+const score_path : String = "user://likearecordscore.txt"
+
+func _ready() -> void:
+	var file := FileAccess.open(score_path, FileAccess.READ_WRITE)
+	if not file:
+		print(FileAccess.get_open_error()) 
+		return
+	hs = file.get_32()
+	file.close()
+
 func reset() -> void:
 	if score > hs:
 		hs = score
 	score = 0
 	wave = 1
+	
+	
+	var file := FileAccess.open(score_path, FileAccess.WRITE)
+	if not file: return
+	file.store_32(hs)
+	file.close()
 
 func add_score(value: int) -> void:
 	score += value
